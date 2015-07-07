c9b9b3c08380dd87244ead0fe16db7586cf72ffb
ll521life <403956831@qq.com>
2015-07-07 17:52:47 +0800

    完整工程

---
+++ hh/hh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hh
+//
+//  Created by qianfeng on 15/7/7.
+//  Copyright (c) 2015年 liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hh/hh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hh
+//
+//  Created by qianfeng on 15/7/7.
+//  Copyright (c) 2015年 liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
